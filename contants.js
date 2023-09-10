0f0b4691b580b1c70e92469e4229dfe39a126d0b
penta-01 <120293238+penta-01@users.noreply.github.com>
2023-09-10 17:14:24 +0900

    Add files via upload

---
+++ contants.js
@@ -0,0 +1,9 @@
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 40;
+let canvas;
+let ctx;
+let grid;
+let block;
+let color;
+let gravity = 0;
---
+++ main.js
@@ -0,0 +1,112 @@
+window.onload = function () {
+  init();
+  draw();
+};
+
+function init() {
+  canvas = document.getElementById('board');
+  ctx = canvas.getContext('2d');
+
+  ctx.canvas.width = COLS * BLOCK_SIZE;
+  ctx.canvas.height = ROWS * BLOCK_SIZE;
+  ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+}
+
+function draw() {
+  drawGrid();
+}
+
+function drawGrid() {
+  ctx.strokeStyle = '#ddd';
+  ctx.lineWidth = 0.05;
+
+  for (var i = 1; i < COLS; i++) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, ROWS);
+    ctx.stroke();
+  }
+  for (var i = 1; i < ROWS; i++) {
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(COLS, i);
+    ctx.stroke();
+  }
+}
+
+function resetBoard() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, COLS, ROWS);
+  drawGrid();
+}
+
+function start() {
+  spawnGrid();
+  spawnBlock();
+  setInterval(() => {
+    resetBoard();
+    drawBlock();
+  }, 1);
+}
+
+function spawnGrid() {
+  grid = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+}
+
+function spawnBlock() {
+  ran = Math.floor(Math.random() * 7 + 1);
+  switch (ran) {
+    case 1:
+      block = OBLOCK;
+      color = 'yellow';
+      break;
+    case 2:
+      block = IBLOCK;
+      color = 'aqua';
+      break;
+    case 3:
+      block = JBLOCK;
+      color = 'blue';
+      break;
+    case 4:
+      block = LBLOCK;
+      color = 'orange';
+      break;
+    case 5:
+      block = SBLOCK;
+      color = 'greenyellow';
+      break;
+    case 6:
+      block = ZBLOCK;
+      color = 'red';
+      break;
+    case 7:
+      block = TBLOCK;
+      color = 'darkviolet';
+      break;
+  }
+
+  ctx.fillStyle = color;
+  block.forEach((arr, y) => {
+    arr.forEach((num, x) => {
+      if (num == 1) {
+        ctx.fillRect(x + 3, y, 1, 1);
+      }
+    });
+  });
+}
+
+function drawBlock() {
+  let g = Math.floor(gravity);
+  ctx.fillStyle = color;
+  block.forEach((arr, y) => {
+    arr.forEach((num, x) => {
+      if (num == 1) {
+        ctx.fillRect(x + 3, y + g, 1, 1);
+      }
+    });
+  });
+  gravity += 0.005;
+}
+
+function move() {}
