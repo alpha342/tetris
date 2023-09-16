c811f94e45f02c6c6fa1fe296080b2f6a3ea70c5
penta-01 <120293238+penta-01@users.noreply.github.com>
2023-09-16 15:55:37 +0900

    Add files via upload

--- contants.js
+++ contants.js
@@ -1,9 +1,12 @@
 const COLS = 10;
 const ROWS = 20;
 const BLOCK_SIZE = 40;
+
 let canvas;
 let ctx;
-let grid;
 let block;
 let color;
 let gravity = 0;
+let canMove = false;
+let yPos = 0;
+let stoppedTime = 0;
--- main.js
+++ main.js
@@ -13,44 +13,16 @@ function init() {
 }
 
 function draw() {
-  drawGrid();
-}
-
-function drawGrid() {
-  ctx.strokeStyle = '#ddd';
-  ctx.lineWidth = 0.05;
-
-  for (var i = 1; i < COLS; i++) {
-    ctx.beginPath();
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, ROWS);
-    ctx.stroke();
-  }
-  for (var i = 1; i < ROWS; i++) {
-    ctx.beginPath();
-    ctx.moveTo(0, i);
-    ctx.lineTo(COLS, i);
-    ctx.stroke();
-  }
-}
-
-function resetBoard() {
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, COLS, ROWS);
-  drawGrid();
-}
-
-function start() {
-  spawnGrid();
   spawnBlock();
   setInterval(() => {
-    resetBoard();
+    reset();
     drawBlock();
   }, 1);
 }
 
-function spawnGrid() {
-  grid = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+function reset() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, 20, 40);
 }
 
 function spawnBlock() {
@@ -58,7 +30,7 @@ function spawnBlock() {
   switch (ran) {
     case 1:
       block = OBLOCK;
-      color = 'yellow';
+      color = 'gold';
       break;
     case 2:
       block = IBLOCK;
@@ -87,9 +59,9 @@ function spawnBlock() {
   }
 
   ctx.fillStyle = color;
-  block.forEach((arr, y) => {
-    arr.forEach((num, x) => {
-      if (num == 1) {
+  block.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value == 1) {
         ctx.fillRect(x + 3, y - 2, 1, 1);
       }
     });
@@ -99,14 +71,27 @@ function spawnBlock() {
 function drawBlock() {
   let g = Math.floor(gravity);
   ctx.fillStyle = color;
-  block.forEach((arr, y) => {
-    arr.forEach((num, x) => {
-      if (num == 1) {
-        ctx.fillRect(x + 3, y + g - 2, 1, 1);
+  block.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value == 1) {
+        ctx.fillRect(x + 3, y - 2 + g, 1, 1);
+        if (yPos != y + g - 2) yPos = y + g - 2;
       }
     });
   });
-  gravity += 0.005;
+  if (yPos == 19) {
+    canMove = false;
+    stoppedTime += 1;
+  }
+  if (stoppedTime == 250) {
+    canMove = true;
+    stoppedTime = 0;
+    gravity = 0;
+    spawnBlock();
+  }
+  if (canMove) gravity += 0.01;
 }
 
-function move() {}
+function start() {
+  canMove = !canMove;
+}
