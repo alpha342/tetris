46bce06511c8d0834e0a40bc6976b68ab42a3770
penta-01 <120293238+penta-01@users.noreply.github.com>
2023-09-17 18:49:27 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,119 +4,136 @@ window.onload = function () {
 };
 
 function init() {
+  grid = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+  newGrid = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
   canvas = document.getElementById('board');
   ctx = canvas.getContext('2d');
+  gravity = 0;
+  canMove = true;
+  pause = false;
+  yPos = 0;
+  stoppedTime = 0;
 
   ctx.canvas.width = COLS * BLOCK_SIZE;
   ctx.canvas.height = ROWS * BLOCK_SIZE;
   ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
-  spawnGrid();
 }
 
 function draw() {
+  resetGrid();
   spawnBlock();
   setInterval(() => {
-    reset();
+    resetGrid();
+    fallingBlock();
     drawBlock();
-  }, 1);
+  }, 250);
+  setInterval(() => {
+    location.reload(true);
+  }, 60000);
 }
 
-function spawnGrid() {
-  grid = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
-}
-
-function reset() {
+function resetGrid() {
+  newGrid.forEach((arr, y) => {
+    arr.forEach((num, x) => {
+      grid[y][x] = num;
+    });
+  });
   ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, 10, 20);
-  grid.forEach((row, y) => {
-    ctx.fillStyle = 'black';
-    row.forEach((value, x) => {
-      if (value >= 1) {
-        if (value == 1) ctx.fillStyle = 'red';
-        if (value == 2) ctx.fillStyle = 'orange';
-        if (value == 3) ctx.fillStyle = 'gold';
-        if (value == 4) ctx.fillStyle = 'green';
-        if (value == 5) ctx.fillStyle = 'aqua';
-        if (value == 6) ctx.fillStyle = 'blue';
-        if (value == 7) ctx.fillStyle = 'darkviolet';
+  grid.forEach((arr, y) => {
+    arr.forEach((num, x) => {
+      if (num == 0) {
         ctx.fillRect(x, y, 1, 1);
-        ctx.fillStyle = 'black';
       }
     });
   });
 }
 
-http: function spawnBlock() {
-  ran = Math.floor(Math.random() * 7 + 1);
-  switch (ran) {
-    case 1:
-      block = OBLOCK;
-      color = 'gold';
-      break;
-    case 2:
-      block = IBLOCK;
-      color = 'aqua';
-      break;
-    case 3:
-      block = JBLOCK;
-      color = 'blue';
-      break;
-    case 4:
-      block = LBLOCK;
-      color = 'orange';
-      break;
-    case 5:
-      block = SBLOCK;
-      color = 'green';
-      break;
-    case 6:
-      block = ZBLOCK;
-      color = 'red';
-      break;
-    case 7:
-      block = TBLOCK;
-      color = 'darkviolet';
-      break;
+function spawnBlock() {
+  let ran = Math.floor(Math.random() * 7 + 1);
+  if (ran == 1) {
+    block = ZBLOCK;
+    color = 'red';
   }
-
-  ctx.fillStyle = color;
-  block.forEach((row, y) => {
-    row.forEach((value, x) => {
-      if (value == 1) {
-        ctx.fillRect(x + 3, y - 2, 1, 1);
+  if (ran == 2) {
+    block = LBLOCK;
+    color = 'orange';
+  }
+  if (ran == 3) {
+    block = OBLOCK;
+    color = 'gold';
+  }
+  if (ran == 4) {
+    block = SBLOCK;
+    color = 'green';
+  }
+  if (ran == 5) {
+    block = IBLOCK;
+    color = 'aqua';
+  }
+  if (ran == 6) {
+    block = JBLOCK;
+    color = 'blue';
+  }
+  if (ran == 7) {
+    block = TBLOCK;
+    color = 'darkviolet';
+  }
+  block.forEach((arr, y) => {
+    arr.forEach((num, x) => {
+      if (num > 0 && y - 2 >= 0) {
+        grid[y - 2][x + 3] = num;
       }
     });
   });
+}
+
+function fallingBlock() {
+  if (!pause && canMove && yPos <= 18) gravity += 1;
+  let g = Math.floor(gravity);
+  block.forEach((arr, y) => {
+    arr.forEach((num, x) => {
+      if (num > 0 && y + g - 2 >= 0) {
+        grid[y + g - 2][x + 3] = num;
+        if (yPos <= y + g - 2) yPos = y + g - 2;
+        if (yPos != 19 && grid[yPos + 1][x + 3] != 0) canMove = false;
+      }
+    });
+  });
+  if (stoppedTime == 4) {
+    canMove = true;
+    yPos = 0;
+    stoppedTime = 0;
+    gravity = 0;
+    grid.forEach((arr, y) => {
+      arr.forEach((num, x) => {
+        newGrid[y][x] = num;
+      });
+    });
+    spawnBlock();
+  }
+  if (yPos == 19) canMove = false;
+  if (!canMove) stoppedTime += 1;
 }
 
 function drawBlock() {
-  let g = Math.floor(gravity);
-  ctx.fillStyle = color;
-  block.forEach((row, y) => {
-    row.forEach((value, x) => {
-      if (value >= 1) {
-        ctx.fillRect(x + 3, y - 2 + g, 1, 1);
-        if (yPos <= y + g - 2) yPos = y + g - 2;
-        if (yPos == 19) {
-          grid[y + g - 2][x + 3] = value;
-        }
+  grid.forEach((arr, y) => {
+    arr.forEach((num, x) => {
+      if (num > 0) {
+        if (num == 1) color = 'red';
+        if (num == 2) color = 'orange';
+        if (num == 3) color = 'gold';
+        if (num == 4) color = 'green';
+        if (num == 5) color = 'aqua';
+        if (num == 6) color = 'blue';
+        if (num == 7) color = 'darkviolet';
+
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, 1, 1);
       }
     });
   });
-  if (yPos == 19) {
-    canMove = false;
-    stoppedTime += 1;
-  }
-  if (stoppedTime == 100 /*800 */) {
-    yPos = 0;
-    canMove = true;
-    stoppedTime = 0;
-    gravity = 0;
-    spawnBlock();
-  }
-  if (canMove) gravity += 0.05 /*0.004 */;
 }
 
 function start() {
-  canMove = !canMove;
+  pause = !pause;
 }
